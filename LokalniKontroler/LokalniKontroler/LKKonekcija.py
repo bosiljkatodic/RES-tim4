978a766a5ff92a1eccc1790c094cd7e6fc22fcf6
jtrivun16 <trivun_2000@rocketmail.com>
2022-06-05 23:28:20 +0200

    #367

--- LokalniKontroler/LokalniKontroler/LKKonekcija.py
+++ LokalniKontroler/LokalniKontroler/LKKonekcija.py
@@ -6,3 +6,9 @@ if __name__== "__main__":
     server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #klijentska str tcp
     server.connect((socket.gethostbyname(), 4000))
+
+    #serverska strana tcp
+    server2=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server2.bind((socket.gethostbyname(),5000))
+
+    server2.listen(10);

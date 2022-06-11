853fd1602e02a36d4575855ac12c9f9ffd463b80
Bosiljka Todic <bosiljkatodic00@gmail.com>
2022-06-11 12:14:35 +0200

    #583 Promene za izabrani period

--- Asset Management Sistem/Asset Management Sistem/database.py
+++ Asset Management Sistem/Asset Management Sistem/database.py
@@ -1,9 +1,10 @@
 from multiprocessing import connection
 import sqlite3
 
-CREATE_DEVICES_TABLE = "CREATE TABLE IF NOT EXISTS devices (localDeviceCode TEXT PRIMARY KEY, timestamp TIMESTAMP, actualValue INTEGER, state TEXT);"
+CREATE_DEVICES_TABLE = "CREATE TABLE IF NOT EXISTS devices (localDeviceCode TEXT PRIMARY KEY, timestamp DATETIME, actualValue INTEGER, state TEXT);"
 INSERT_DEVICE = "INSERT INTO devices (localDeviceCode, timestamp, actualValue, state) VALUES (?, ?, ?, ?);"
 GET_ALL_DEVICES = "SELECT * FROM devices;"
+GET_CHANGES_BY_LOCAL_DEVICE_CODE_AND_PERIOD = "SELECT * FROM devices WHERE localDeviceCode = ? AND timestamp >= ? AND timestamp <= ?;"
 
 def connect():
     return sqlite3.connect("data.db")
@@ -20,4 +21,7 @@ def get_all_devices(connection):
     with connection:
         return connection.execute(GET_ALL_DEVICES).fetchall()
 
+def get_changes_by_localDeviceCode(connection, localDeviceCode, timestampStart, timestampStop):
+    with connection:
+        return connection.execute(GET_CHANGES_BY_LOCAL_DEVICE_CODE_AND_PERIOD, (localDeviceCode,timestampStart, timestampStop, )).fetchall()
 
